8603b2e8ad3f8d2e31a7fffd09f64333bd63af75
Mehul Agarwal <mehula@andrew.cmu.edu>
2022-03-12 03:10:46 -0500

    First commit

--- aave-flashloan/scripts/deployment_v2.py
+++ aave-flashloan/scripts/deployment_v2.py
@@ -1,4 +1,4 @@
-from brownie import FlashloanV2, FlashloanWind, accounts, config, network
+from brownie import FlashloanV2, accounts, config, network
 
 # AAVE_LENDING_POOL_ADDRESS_PROVIDER = "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5"
 
@@ -12,12 +12,7 @@ def main():
         config["wallets"]["from_key"]
     )  # add your keystore ID as an argument to this call
 
-    # flashloan = FlashloanV2.deploy(
-    #     config["networks"][network.show_active()]["aave_lending_pool_v2"],
-    #     {"from": acct},
-    # )
-    # return flashloan
-    flashloan = FlashloanWind.deploy(
+    flashloan = FlashloanV2.deploy(
         config["networks"][network.show_active()]["aave_lending_pool_v2"],
         {"from": acct},
     )
--- aave-flashloan/scripts/run_flash_loan_v2.py
+++ aave-flashloan/scripts/run_flash_loan_v2.py
@@ -1,6 +1,6 @@
-from brownie import FlashloanV2, FlashloanWind, accounts, config, network, interface
+from brownie import FlashloanV2, accounts, config, network, interface
 
-MINIMUM_FLASHLOAN_WETH_BALANCE = 200000000000000000
+MINIMUM_FLASHLOAN_WETH_BALANCE = 500000000000000000
 ETHERSCAN_TX_URL = "https://kovan.etherscan.io/tx/{}"
 
 
@@ -10,13 +10,12 @@ def main():
     """
     acct = accounts.add(config["wallets"]["from_key"])
     print("Getting Flashloan contract...")
-    # flashloan = FlashloanV2[len(FlashloanV2) - 1]
-    flashloan = FlashloanWind[len(FlashloanV2) - 1]
+    flashloan = FlashloanV2[len(FlashloanV2) - 1]
     weth = interface.WethInterface(config["networks"][network.show_active()]["weth"])
     # We need to fund it if it doesn't have any token to fund!
     if weth.balanceOf(flashloan) < MINIMUM_FLASHLOAN_WETH_BALANCE:
         print("Funding Flashloan contract with WETH...")
-        weth.transfer(flashloan, 2000000000000000000, {"from": acct})
+        weth.transfer(flashloan, "1 ether", {"from": acct})
     print("Executing Flashloan...")
     tx = flashloan.flashloan(weth, {"from": acct})
     print("You did it! View your tx here: " + ETHERSCAN_TX_URL.format(tx.txid))
